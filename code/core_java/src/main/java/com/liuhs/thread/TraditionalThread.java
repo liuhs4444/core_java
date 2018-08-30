4ed31086364e372d01085f0e7eb157d1f4aa9b5f
liuhs4444 <liuhs4444@163.com>
2018-08-31 02:04:40 +0800

    传统线程的创建方式

---
+++ code/core_java/src/main/java/com/liuhs/thread/TraditionalThread.java
@@ -0,0 +1,40 @@
+package com.liuhs.thread;
+
+public class TraditionalThread {
+    public static void main(String args[]) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("1:" + Thread.currentThread().getName());
+                    System.out.println("2:" + this.getName());
+                }
+            }
+        };
+        thread.start();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("1:" + Thread.currentThread().getName());
+                    System.out.println("2:" + Thread.currentThread().getName());
+                }
+            }
+        });
+        thread1.start();
+    }
+}
+
+
+
