feb85c036997769d63238153c00035afeaaff5cc
liuhs4444 <liuhs4444@163.com>
2018-09-07 07:12:18 +0800

    线程变量绑定，use map

---
+++ code/core_java/src/main/java/com/liuhs/thread/local/ThreadScopeShareData1.java
@@ -0,0 +1,42 @@
+package com.liuhs.thread.local;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class ThreadScopeShareData1 {
+
+    // private static int data = 0;
+
+    private static Map<Thread, Integer> threadData = new HashMap<>();
+
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 2; i++) {
+            new Thread(() -> {
+                int data = new Random().nextInt();
+                System.out.println(Thread.currentThread().getName() + " has put data : " + data);
+
+                threadData.put(Thread.currentThread(), data);
+
+                new A().get();
+                new B().get();
+            }).start();
+        }
+    }
+
+    static class A {
+        public void get() {
+            int data = threadData.get(Thread.currentThread());
+            System.out.println("A from " + Thread.currentThread().getName() + " get data:" + data);
+        }
+    }
+
+    static class B {
+        public void get() {
+            int data = threadData.get(Thread.currentThread());
+            System.out.println("B from " + Thread.currentThread().getName() + " get data:" + data);
+        }
+    }
+
+}
